8ddd01e8de8a814870c644db1ff1061aae784ee1
Duong Pham <pkduong@gmail.com>
2025-12-31 17:00:17 +0700

    Correct test case search assertion from text input, and update .gitignore file.

--- tests/search/search-laptops-category.spec.ts
+++ tests/search/search-laptops-category.spec.ts
@@ -9,8 +9,8 @@ test.describe('Product Search and Filtering Tests', () => {
     // Arrange
     const category = 'Laptops';
     const laptopProducts = [
-      'Sony vaio i5',
-      'Sony vaio i7'
+      { name: 'Sony vaio i5', price: '$790' },
+      { name: 'MacBook air', price: '$700' }
     ];
 
     // Act & Assert
@@ -22,22 +22,20 @@ test.describe('Product Search and Filtering Tests', () => {
       await page.waitForTimeout(Env.WAITING_TIME);
     });
 
-    await test.step("Verify that laptop products are displayed with names, prices, and descriptions", async () => {
+    await test.step("Verify that laptop products are displayed with correct names and prices", async () => {
       for (const product of laptopProducts) {
-        const productLink = page.getByRole('link', { name: product, exact: true });
+        const productLink = page.getByRole('link', { name: product.name, exact: true });
         await expect(productLink).toBeVisible();
 
         // Get the locator of the product card
         const productCard = page.locator('div.card-block').filter({ has: productLink });
 
-        // Verify prices are displayed for products
-        const prices = productCard.locator('h5');
-        const pricesCount = await prices.count();
-        expect.soft(pricesCount).toBeGreaterThan(0);
+        // Verify product name is displayed
+        await expect.soft(productLink).toHaveText(product.name);
 
-        // Verify descriptions are displayed
-        const descriptions = productCard.locator('p#article');
-        expect.soft(descriptions).not.toBeEmpty();
+        // Verify price is displayed and matches expected price
+        const priceElement = productCard.locator('h5');
+        await expect.soft(priceElement).toContainText(product.price);
       }
     });
   });
